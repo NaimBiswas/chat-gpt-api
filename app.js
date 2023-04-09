b69b9d3533d76950d64a1ecf5a465a061d50678c
NaimBiswas <nayeembiswas2@gmail.com>
2023-04-09 22:27:50 +0600

    Chat GPT backend model with davinci model

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const morgan = require("morgan")
+const cors = require("cors")
+require("dotenv").config();
+var bodyParser = require('body-parser')
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.json())
+
+const { Configuration, OpenAIApi }  =  require("openai");
+const configuration = new Configuration({
+    organization: process.env.CHAT_ORG,
+    apiKey: process.env.CHAT_GPT_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+app.post("/chat", async (req, res) => {
+    const { prompt, ip } = req.body
+    if(!prompt || !ip) {
+        return res.status(400).json({ message: "Invalid request", success:false })
+    }
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 500,
+        temperature: 0,
+    });
+    res.status(200).json({
+        success:true,
+        data:response?.data,
+        message: response?.data?.choices[0]?.text ?? response.data,
+        date: new Date().toISOString()
+    })
+})
+app.get("/", (req, res) =>  res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Chat GPT server is listening on port ${port}!`));
